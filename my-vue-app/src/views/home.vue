e523b8b2f45c9266c6c78cce8d2e1c9a9024bd08
maqun520 <765463770@qq.com>
2021-08-27 16:39:03 +0800

    home.vue

--- my-vue-app/src/views/home.vue
+++ my-vue-app/src/views/home.vue
@@ -36,7 +36,7 @@ onMounted(() => {//生命周期
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
     Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    <code>home.vue</code> to test hot module replacement.
   </p>
 </template>
 
